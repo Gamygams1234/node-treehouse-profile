32af817bf581950d44203fbc288590bd25eba7a2
Gamygams1234 <gamyburgos@gmail.com>
2020-08-07 20:01:25 -0700

    Making a router file

--- app.js
+++ app.js
@@ -4,36 +4,18 @@
 //1. Create a web server
 
 var http = require("http");
+var router = require("./router.js");
+
 http
   .createServer(function (request, response) {
-    homeRoute(request, response);
-    userRoute(request, response);
+    router.home(request, response);
+    router.user(request, response);
   })
   .listen(3000);
 console.log("Server running at http://<workspace-url>/");
 
 //2. Handle HTTP route GET / and POST / i.e. Home
 
-function homeRoute(request, response) {
-  if (request.url === "/") {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-
-    response.write("Header\n");
-    response.write("Search\n");
-    response.end("Footer\n");
-  }
-}
-
-function userRoute(request, response) {
-  var username = request.url.replace("/", "");
-  if (username.length > 0) {
-    response.writeHead(200, { "Content-Type": "text/plain" });
-
-    response.write("Header\n");
-    response.write(username + "\n");
-    response.end("Footer\n");
-  }
-}
 //if url == "/" && GET
 //show search
 //if url == "/" && POST
