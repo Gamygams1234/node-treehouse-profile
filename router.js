8bae9a0a0888c332ba9e6bb03c1de5168eb9df1c
Gamygams1234 <gamyburgos@gmail.com>
2020-08-07 20:33:31 -0700

    Population User Information

--- router.js
+++ router.js
@@ -1,3 +1,5 @@
+var Profile = require("./profile.js");
+
 function home(request, response) {
   if (request.url === "/") {
     response.writeHead(200, { "Content-Type": "text/plain" });
@@ -13,9 +15,24 @@ function user(request, response) {
   if (username.length > 0) {
     response.writeHead(200, { "Content-Type": "text/plain" });
 
-    response.write("Header\n");
-    response.write(username + "\n");
-    response.end("Footer\n");
+    var studentProfile = new Profile(username);
+
+    studentProfile.on("end", function (profileJSON) {
+      var values = {
+        avatarUrl: profileJSON.gravatar_url,
+        username: profileJSON.profile_name,
+        // the badges is an array
+        badges: profileJSON.badges.length,
+        javascriptPoints: profileJSON.points.JavaScript,
+      };
+      response.write(values.username + " has " + values.badges + "\n");
+      response.end(username + "\n");
+    });
+
+    studentProfile.on("error", function (error) {
+      response.write(error.message);
+      response.end(username + "\n");
+    });
   }
 }
 
